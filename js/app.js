2a025d75f79a5048d4fd315c373fe77e39873fe6
Nataliia Frank <nataliiafrank@gmail.com>
2020-06-11 22:38:15 -0700

    initial setup

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Defining global veriables
+const bp = {
+	mobile: 320,
+	tablet: 768,
+	desktop: 1024,
+	largeDesctop: 1440
+}
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+const state = {
+	active: 'is-active',
+	desabled: 'is-disabled',
+	valid: 'is-valid',
+	invalid: 'is-invalid'
+}
+
+const domSelectors = {
+}
