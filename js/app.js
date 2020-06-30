0a2df34eba7c76139f46024a8b7e3364b99b5e69
Nataliia Frank <nataliiafrank@gmail.com>
2020-06-30 15:39:10 -0700

    comtinue markup - projects section

--- js/app.js
+++ js/app.js
@@ -14,12 +14,16 @@ const state = {
 	desabled: 'is-disabled',
 	valid: 'is-valid',
 	invalid: 'is-invalid',
-	open: 'is-open'
+	open: 'is-open',
+	hidden: 'is-hidden'
 }
 
 const domSelectors = {
 	toggleMenuButton: document.querySelector('.js-toggle-menu'),
 	headerNav: document.querySelector('.js-header-nav'),
+	popup: document.querySelector('.js-popup'),
+	popuplink: document.querySelectorAll('.js-popup-link'),
+	popupClose: document.querySelectorAll('.js-popup-close'),
 }
 
 const toggleMenu = (event) => {
@@ -29,5 +33,37 @@ const toggleMenu = (event) => {
 	domSelectors.headerNav.classList.toggle(state.open);
 }
 
+const togglePopup = (currentPopup) => {
+	currentPopup.classList.toggle(state.hidden);
+	currentPopup.classList.toggle(state.open);
+
+	domSelectors.popup.classList.toggle(state.hidden);
+}
+
+const handlePopup = (event) => {
+	const currentLink = event.target;
+	let currentVal;
+
+	if (!currentLink.getAttribute('data-project')) {
+		currentVal = currentLink.closest('.js-popup-item').id;
+	} else {
+		currentVal = currentLink.dataset.project;
+	}
+
+	const currentPopup = document.querySelector(`#${currentVal}`);
+	togglePopup(currentPopup);
+}
+
 // Event Listenres
 domSelectors.toggleMenuButton.addEventListener('click', toggleMenu);
+domSelectors.popuplink.forEach(function(item) {
+	item.addEventListener('click', (e) => {
+		handlePopup(e);
+	});
+});
+
+domSelectors.popupClose.forEach(function(item) {
+	item.addEventListener('click', (e) => {
+		handlePopup(e);
+	});
+});
